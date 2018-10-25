fc6e3dfc2dc79e508964fbdab339a6ac541c08f8
Andy Bowskill <andybowskill@gmail.com>
2018-10-25 17:30:52 +0100

    Tidy-up the code.

--- speedtestform.py
+++ speedtestform.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import subprocess
+import socket
 
 from tkinter import *
 from tkinter import font
-import subprocess
-import socket
+
 
 class SpeedTestForm:
     def __init__(self, master):
@@ -17,31 +20,31 @@ class SpeedTestForm:
         form_font = font.Font(family='Arial', size=24, weight=font.NORMAL)
         orange_code = '#dd5202'
 
-        self.IP_text = StringVar()
-        self.IP_text.set('')
+        self.ip_text = StringVar()
+        self.ip_text.set('')
 
         self.network_text = StringVar()
         self.network_text.set('')
 
-        self.IP_label = Label(master, font=form_font, textvariable=self.IP_text)
-        self.IP_label.config(fg=orange_code, bg='black')
-        self.IP_label.pack(padx=0, pady=10, fill=X)
+        self.ip_label = Label(master, font=form_font, textvariable=self.ip_text)
+        self.ip_label.config(fg=orange_code, bg='black')
+        self.ip_label.pack(padx=0, pady=10, fill=X)
 
         self.network_label = Label(master, font=form_font, textvariable=self.network_text)
         self.network_label.config(fg=orange_code, bg='black', height=4)
         self.network_label.pack(padx=0, pady=6, fill=X)
 
-        self.check_button = Button(master, text='Check Network', font=form_font, command=lambda: self.run_speedtest_process())
+        self.check_button = Button(master, text='Check Network', font=form_font, command=lambda: self.check_network())
         self.check_button.config(fg=orange_code, bg='black')
         self.check_button.pack(padx=0, pady=10, fill=BOTH)
 
-    def run_speedtest_process(self):
+    def check_network(self):
         self.network_text.set('Checking...')
         self.check_button.config(state=DISABLED)
 
-        self.IP_text.set(self.read_IP())
+        self.ip_text.set(self.read_ip())
 
-        def run_subprocess():
+        def run_speedtest():
             response = subprocess.Popen('/usr/local/bin/speedtest-cli --simple', shell=True, stdout=subprocess.PIPE).stdout.read()
             response_string = response.decode('utf-8')
             if not response_string:
@@ -50,19 +53,26 @@ class SpeedTestForm:
             self.network_text.set(response_string)
             self.check_button.config(state=NORMAL)
 
-        self.master.after(500, run_subprocess)
+        self.master.after(500, run_speedtest)
 
-    def read_IP(self):
+    @staticmethod
+    def read_ip():
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             s.connect(("8.8.8.8", 80))
-            IP = 'IP: ' + s.getsockname()[0]
+            ip = 'IP: ' + s.getsockname()[0]
         except socket.error:
-            IP = 'No IP found.'
+            ip = 'No IP found.'
         finally:
             s.close()
-        return IP
+        return ip
 
-root = Tk()
-speed_test_form = SpeedTestForm(root)
-root.mainloop()
+
+def main():
+    root = Tk()
+    SpeedTestForm(root)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
